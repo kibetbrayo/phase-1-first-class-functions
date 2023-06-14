3fd95e70c7289a97fc2b1734853e108b287607cc
iam <brian.kibet2@student.moringaschool.com>
2023-06-14 13:23:53 +0000

    completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function  receivesAFunction(callback){
+    return callback ();
+}
+function  returnsANamedFunction(){
+    function namedFunction() {
+        
+    }
+    return namedFunction ;
+
+    }
+
+    function returnsAnAnonymousFunction(){
+        return function() {
+
+        };
+    }
+
